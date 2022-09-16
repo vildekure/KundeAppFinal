aed696a9437db538df04b5d546d4b10fa2965573
Vilde <69961376+vildekure@users.noreply.github.com>
2022-09-16 11:16:37 +0200

    Added endre, lagre html and lagre js

---
+++ KundeAppFinal/wwwroot/js/lagre.js
@@ -0,0 +1,15 @@
+﻿function lagreKunde() {
+    const kunde = {
+        navn: $("#navn").val(),
+        adresse: $("#adresse").val()
+    }
+    const url = "Kunde/Lagre";
+    $.post(url, kunde, function (OK) {
+        if (OK) {
+            window.location.href = 'index.html';
+        }
+        else {
+            $("#feil").html("Feil i db - prøv igjen senere");
+        }
+    });
+};
