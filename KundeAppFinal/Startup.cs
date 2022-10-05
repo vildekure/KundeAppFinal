30e3334a6a99959f558d660e17ca420209b181be
Vilde <69961376+vildekure@users.noreply.github.com>
2022-10-05 13:09:53 +0200

    Before DAL implement

--- KundeAppFinal/Startup.cs
+++ KundeAppFinal/Startup.cs
@@ -36,6 +36,7 @@ namespace KundeAppFinal
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                DBInit.Initializer(app); //Denne må fjernes hvis vi vil beholde dataene i databasen og ikke initialisere
             }
 
             app.UseRouting();
