979846792e5b3621c5d2dabfd7e1b83dccf7ed96
Vilde <69961376+vildekure@users.noreply.github.com>
2022-10-11 14:16:31 +0200

    Lagd alle valideringer i validering.js

--- KundeAppFinal/wwwroot/js/validering.js
+++ KundeAppFinal/wwwroot/js/validering.js
@@ -24,4 +24,44 @@ function validerEtternavn(etternavn) {
     }
 }
 
+function validerAdresse(adresse) {
+    const regexp = /^[0-9a-zA-ZæøåÆØÅ\.\ \-]{2,50}$/;
+    const ok = regexp.test(adresse);
+    if (!ok) {
+        $("#feilAdresse").html("Adressen må bestå av 2 til 50 bokstaver og tall");
+        return false;
+    }
+    else {
+        $("#feilAdresse").html("");
+        return true;
+    }
+}
+
+function validerPostnr(postnr) {
+    const regexp = /^[0-9]{4}$/;
+    const ok = regexp.test(postnr);
+    if (!ok) {
+        $("#feilPostnr").html("Postnummer må bestå av 4 tall");
+        return false;
+    }
+    else {
+        $("#feilPostnr").html("");
+        return true;
+    }
+}
+
+function validerPoststed(poststed) {
+    const regexp = /^[a-zA-ZæøåÆØÅ\.\ \-]{2,50}$/;
+    const ok = regexp.test(poststed);
+    if (!ok) {
+        $("#feilPoststed").html("Etternavnet må bestå av 2 til 20 bokstaver");
+        return false;
+    }
+    else {
+        $("#feilPoststed").html("");
+        return true;
+    }
+}
+
+
 
